d66721d08f3182f893e2b9520072c66a8fb3545d
Rotanak <chanrotanak.kong@gmail.com>
2022-08-01 08:35:43 +0700

    done homepage layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,35 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <!DOCTYPE html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"content="IE=edge">
-        <title>tickets_home</title>
-        <meta name="description"content="">
-        <meta name="viewport"content="width-device-width,initial-scale=1">
-        <link rel="stylesheet"href="../css/main.css">
-    </head>
-
-    <body>
-        <nav>
-            <ul class="nav_links">
-                <li><a href="#"> home </a></li>
-                <li><a href="#"> showtime </a></li>
-                <li><a href="#">comming soon</a></li>
-                <li><a href="#"> contact us </a></li>
-            </ul>
-        </nav>
-        {{-- <form class="slide">
-            <img class="mySlides" src="../img/batman1.jpg">
-            <img class="mySlides" src="../img/conjuring2a.jpg">
+    <div class="body-content">
+        <div class="slide">
+            <img class="slides" src="{{asset('/img/batman1.jpg') }}" alt="">
+            {{-- <img class="slides" src="../img/conjuring2a.jpg"> --}}
             <script>
                 var myIndex = 0;
                 carousel();
 
                 function carousel() {
                     var i;
-                    var x = document.getElementsByClassName("mySlides");
+                    var x = document.getElementsByClassName("slides");
                     for (i = 0; i < x.length; i++) {
                         x[i].style.display = "none";
                     }
@@ -38,23 +20,31 @@
                         myIndex = 1
                     }
                     x[myIndex - 1].style.display = "block";
-                    setTimeout(carousel, 1000); // Change image every 2 seconds
+                    setTimeout(carousel, 1500); // Change image every 2 seconds
                 }
             </script>
-        </form> --}}
-
-        <div>
+            
+        </div>
+        <div class="showing">
+          <ul>
+              <li><a href="#"> Now showing </a></li>
+              <li><a href="#"> Coming soon </a></li>
+          </ul>
+      </div>
+        
+    </div>
+    {{-- <div>
             <ul class="showing">
                 <li><a href="#"> Now showing </a></li>
-                <li><a href="#"> comming soon </a></li>
+                <li><a href="#"> Coming soon </a></li>
             </ul>
-        </div>
+        </div> --}}
 
-        <div class="row">
+    {{-- <div class="row">
             <div class="column">
               <div class="card">
                 <h3>Card 1</h3>
-                <img src=".../img/pirate.jpg">
+                <img src="../img/pirate.jpg">
                 <p>Some text</p>
               </div>
             </div>
@@ -82,9 +72,5 @@
                 <p>Some text</p>
               </div>
             </div>
-          </div>
-    </body>
-
-
-    </html>
+          </div> --}}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,82 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<header >
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible"content="IE=edge">
+    <title>tickets_home</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width,initial-scale=1">
+    <link rel="stylesheet" href=" {{ asset('/css/main.css') }} ">
+</head>
 
-    
-     <img class="logo"src="../img/Momo.jpg"alt="logo" href="/homepage" href="{{ url('/homepage') }}">
+<body style="background-color: #554238">
+    <header>
+        <div class="header-top">
+            <div class="bar-top-left">
+                <img src="../img/logo.png"alt="logo"style="cursor: pointer" onclick="window.location.href='/'">
+            </div>
 
-     <div class="cta">
-        
-        <a class="buttonsign" href="{{ route('login') }}"><button class="sign">Sign in/out</button></a>
+            <div class="bar-top-right">
+                <a href="{{ route('login') }}"><button>Sign In/Out</button></a>
+                <a href="{{ route('register') }}"><button class="register">Register</button></a>
+            </div>
 
-        <a href="{{ route('register') }}"><button  class="register">Register</button></a>
-   
+        </div>
+        <div class="header-bottom">
+                <a onclick="window.location.href='/'" style=cursor:pointer>Home</a>
+                <a href="#">Showtime</a>
+                <a href="#">Coming Soon</a>
+                <a href="#">Contact Us</a>
+        </div>
 
-     </div>
-
-     
-</header>
-<body>
-    <div id="app">
-
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        {{-- bootstrap of logo --}}
+    </header>
+    <div class="wrapper-body">
+        @yield('content')
     </div>
+    
+
+    <footer>
+        <div class="top-footer">
+
+            <div class="top-left-footer">
+                <h3>Menu Link</h3>
+
+                <a href="/">Home</a><br>
+                <a href="/">Now Showing</a><br>
+                <a href="/">Coming Soon</a><br>
+                <a href="/">Contact Us</a>
+            </div>
+
+            <div class="top-middle-footer">
+                <h3>About Phoenix Cinema</h3>
+                <p>We provide showtime of movies and you can book for the seat.</p>
+            </div>
+            <div class="top-right-footer">
+                <h3>Connect with Us</h3>
+                <h4>011-575-065</h4>
+                <div class="icon-social">
+                    <img src="../img/facebook_icon.png" style="width:30px; height:30px; cursor: pointer"
+                        onclick="window.location.href='https://www.facebook.com/kong.rotanak.7/'">
+                    <img src="../img/telegram_icon.png" style="width:30px; height:30px; cursor: pointer"
+                        onclick="window.location.href='https://web.telegram.org/z/#467814096'">
+                    <img src="../img/gmail_icon.png" style="width:30px; height:30px; cursor: pointer"
+                        onclick="window.location.href='https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSMVlCTvxqTrbzWjQtKtvKwCZVHlfMBhgPbbSmcsXDhrgxZsVgsPpxmCfldhpRQQjQNkkJNz'">
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <p>Copyright &copy; 2022. Alright reserved.</p>
+
+            <a href="/policy">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <a href="/term">Terms of Conditions</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <a href="/cookie">Cookie Preferences</a>
+        </div>
+    </footer>
+
 </body>
+
 </html>
